c2912cfe5d89d0bdc2f9fb5190b52b3831af440f
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-02-20 00:57:13 -0300

    Fix: findByUsername return type

--- src/main/java/com/myparty/app/repository/UserRepository.java
+++ src/main/java/com/myparty/app/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.myparty.app.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import com.myparty.app.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
 }
