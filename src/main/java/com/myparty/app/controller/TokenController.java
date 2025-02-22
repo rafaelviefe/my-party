767d4ea7798eb1da250c30260c8ba398d1a87260
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-02-21 23:25:05 -0300

    Refactor: Renaming DTOs

--- src/main/java/com/myparty/app/controller/TokenController.java
+++ src/main/java/com/myparty/app/controller/TokenController.java
@@ -10,8 +10,8 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.myparty.app.controller.dto.LoginRequest;
-import com.myparty.app.controller.dto.LoginResponse;
+import com.myparty.app.controller.dto.LoginRequestDto;
+import com.myparty.app.controller.dto.LoginResponseDto;
 import com.myparty.app.service.UserService;
 import jakarta.validation.Valid;
 
@@ -29,11 +29,11 @@ public class TokenController {
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+	public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
 
-		var user = userService.findByUsername(loginRequest.username());
+		var user = userService.findByUsername(loginRequestDto.username());
 
-		if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
+		if (user.isEmpty() || !user.get().isLoginCorrect(loginRequestDto, passwordEncoder)) {
 			throw new BadCredentialsException("username or password is invalid!");
 		}
 
@@ -52,7 +52,7 @@ public class TokenController {
 
 		var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
 
-		return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+		return ResponseEntity.ok(new LoginResponseDto(jwtValue, expiresIn));
 	}
 
 }
