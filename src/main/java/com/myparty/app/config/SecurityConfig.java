83b953d19186f2b8f516582905978dd3f0b51795
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-04-13 23:46:35 -0300

    Feat: Add security scheme annotation for JWT in SecurityConfig

--- src/main/java/com/myparty/app/config/SecurityConfig.java
+++ src/main/java/com/myparty/app/config/SecurityConfig.java
@@ -22,11 +22,16 @@ import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@SecurityScheme(name = SecurityConfig.SECURITY_SCHEME, type = SecuritySchemeType.HTTP, bearerFormat = "JWT" ,scheme = "bearer")
 public class SecurityConfig {
+
+	public static final String SECURITY_SCHEME = "bearerAuth";
 
 	@Value("${jwt.public.key}")
 	private RSAPublicKey publicKey;
